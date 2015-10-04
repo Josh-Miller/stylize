9617e54157fe4ecf3b4e0714bc9a6a73a40ff305
Josh Miller <josh@atendesigngroup.com>
2015-10-04 02:39:23 -0600

    exposed api, moved cli to cli.js

---
+++ cli.js
@@ -0,0 +1,164 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var program = require('commander'),
+    _ = require('lodash'),
+    fs = require('fs-extra'),
+    chokidar = require('chokidar'),
+    childProcess = require('child_process'),
+    stylizeRegression = require('stylize-regression'),
+    chalk = require('chalk'),
+    path = require('path');
+
+// CLI
+var cliCompile = require('./lib/compile'),
+    cliExport = require('./lib/export'),
+    cliInit = require('./lib/init');
+
+var log = console.log.bind(console);
+var projectPath = process.cwd();
+
+// Watch patterns
+var watch = function(params) {
+  log(chalk.green('Watching'));
+  var watchDir = params.watchDir;
+  var cmdPath = process.cwd();
+  var watcher = chokidar.watch(cmdPath + '/src', {
+    usePolling: true,
+    persistent: true,
+    ignored: /[\/\\]\./,
+  });
+
+  var compileParams = {
+    projectPath: cmdPath,
+  }
+  if (params.output) {
+    compileParams.output = params.output;
+  }
+
+  cliCompile.run(compileParams, function() {
+    log(chalk.green('Fin'));
+  });
+
+  watcher.on('change', function(path, stats) {
+    var pathArr = path.split('/');
+    var fileArr = pathArr[pathArr.length - 1].split('.');
+    var fileSuffix = fileArr[fileArr.length - 1];
+
+    if (fileSuffix === 'yml') {
+      console.log(chalk.cyan('Updated', fileArr));
+      cliCompile.run(compileParams, function() {
+        log(chalk.green('Fin'));
+      });
+    } else {
+      var singlePatternParams = {
+        file: path,
+      }
+      if (params.output) {
+        singlePatternParams.output = params.output;
+      }
+      cliCompile.singlePattern(singlePatternParams, function(fileName) {
+        log(chalk.cyan('Updated', fileName));
+      });
+    }
+
+  });
+
+  watcher.on('add', function(path, stats) {
+    var singlePatternParams = {
+      file: path,
+    }
+    if (params.output) {
+      singlePatternParams.output = params.output;
+    }
+    cliCompile.singlePattern(singlePatternParams, function(fileName) {
+      log(chalk.cyan('Added', fileName));
+    });
+  });
+}
+
+program
+  .version('0.0.1');
+
+program
+  .command('init [env]')
+  .description('Builds a new instance of Stylize')
+  .action(function() {
+    log(chalk.green('Initializing new Stylize project...'));
+    cliInit.run();
+  });
+
+program
+  .command('compile [env]')
+  .alias('c')
+  .description('Compile patterns')
+  .option('-w, --watch', 'Watch and compile patterns on change')
+  .option('-p, --path [value]', 'Set the path to your Stylize project root')
+  .option('-o, --output [value]', 'Set an output path')
+  .action(function(env, options) {
+    var mode = options.watch || false;
+
+    if (mode) {
+      var watchDir = {};
+      var params = {
+        watchDir: watchDir,
+      }
+      if (options.output) {
+        params.output = options.output;
+      }
+
+      watch(params);
+    } else {
+      if (options.path) {
+        projectPath = path.join(process.cwd(), options.path);
+      }
+
+      var params = {
+        projectPath: projectPath,
+      }
+
+      if (options.output) {
+        params.output = options.output;
+      }
+console.log('projectPath', projectPath);
+      cliCompile.run(params, function() {
+        log(chalk.green('Fin'));
+      });
+    }
+  });
+
+program
+  .command('regression [env]')
+  .alias('r')
+  .description('Run regression test')
+  .option('-p, --path [value]', 'Set the path to your Stylize project root')
+  .action(function(env, options) {
+    if (options.path) {
+      projectPath = path.join(process.cwd(), options.path);
+    }
+
+    stylizeRegression.get(projectPath, function(patterns) {
+      stylizeRegression.takeScreenshot(patterns);
+    });
+  });
+
+program
+  .command('export [env]')
+  .alias('e')
+  .description('Export patterns')
+  .option('-o, --output [value]', 'Set an output path')
+  .option('-p, --path [value]', 'Set the path to your Stylize project root')
+  .action(function(env, options) {
+    var output = options.output || false;
+
+    if (options.path) {
+      projectPath = path.join(process.cwd(), options.path);
+    }
+
+    cliExport.run(projectPath, function() {
+      log(chalk.green('Fin'));
+    });
+  });
+
+program.parse(process.argv);
--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-#!/usr/bin/env node
-
 'use strict';
 
-var program = require('commander'),
-    _ = require('lodash'),
-    fs = require('fs-extra'),
-    chokidar = require('chokidar'),
-    childProcess = require('child_process'),
+var _ = require('lodash'),
     stylizeRegression = require('stylize-regression'),
     chalk = require('chalk'),
     path = require('path');
@@ -20,155 +14,25 @@ var cliCompile = require('./lib/compile'),
 var log = console.log.bind(console);
 var projectPath = process.cwd();
 
-// Watch patterns
-var watch = function(params) {
-  log(chalk.green('Watching'));
-  var watchDir = params.watchDir;
-  var cmdPath = process.cwd();
-  var watcher = chokidar.watch(cmdPath + '/src', {
-    usePolling: true,
-    persistent: true,
-    ignored: /[\/\\]\./,
-  });
-
-  var compileParams = {
-    projectPath: cmdPath,
-  }
-  if (params.output) {
-    compileParams.output = params.output;
-  }
-
-  cliCompile.run(compileParams, function() {
-    log(chalk.green('Fin'));
-  });
-
-  watcher.on('change', function(path, stats) {
-    var pathArr = path.split('/');
-    var fileArr = pathArr[pathArr.length - 1].split('.');
-    var fileSuffix = fileArr[fileArr.length - 1];
-
-    if (fileSuffix === 'yml') {
-      console.log(chalk.cyan('Updated', fileArr));
-      cliCompile.run(compileParams, function() {
-        log(chalk.green('Fin'));
-      });
-    } else {
-      var singlePatternParams = {
-        file: path,
-      }
-      if (params.output) {
-        singlePatternParams.output = params.output;
-      }
-      cliCompile.singlePattern(singlePatternParams, function(fileName) {
-        log(chalk.cyan('Updated', fileName));
-      });
-    }
-
-  });
-
-  watcher.on('add', function(path, stats) {
-    var singlePatternParams = {
-      file: path,
-    }
-    if (params.output) {
-      singlePatternParams.output = params.output;
-    }
-    cliCompile.singlePattern(singlePatternParams, function(fileName) {
-      log(chalk.cyan('Added', fileName));
+var Stylize = function() {
+  this.compile = function(params) {
+    params.projectPath = path.join(process.cwd(), params.projectPath);
+    cliCompile.run(params, function() {
+      log(chalk.green('Fin'));
     });
-  });
-}
+  };
 
-program
-  .version('0.0.1');
-
-program
-  .command('init [env]')
-  .description('Builds a new instance of Stylize')
-  .action(function() {
-    log(chalk.green('Initializing new Stylize project...'));
-    cliInit.run();
-  });
-
-program
-  .command('compile [env]')
-  .alias('c')
-  .description('Compile patterns')
-  .option('-w, --watch', 'Watch and compile patterns on change')
-  .option('-p, --path [value]', 'Set the path to your Stylize project root')
-  .option('-o, --output [value]', 'Set an output path')
-  .action(function(env, options) {
-    var mode = options.watch || false;
-
-    if (mode) {
-      var watchDir = {};
-      var params = {
-        watchDir: watchDir,
-      }
-      if (options.output) {
-        params.output = options.output;
-      }
-
-      watch(params);
-    } else {
-      if (options.path) {
-        projectPath = path.join(process.cwd(), options.path);
-      }
-
-      var params = {
-        projectPath: projectPath,
-      }
-
-      if (options.output) {
-        params.output = options.output;
-      }
-
-      cliCompile.run(params, function() {
-        log(chalk.green('Fin'));
-      });
-    }
-  });
-
-program
-  .command('build [env]')
-  .alias('b')
-  .description('Build Stylize app')
-  .action(function() {
-    var path = process.cwd();
-    cliBuild.run();
-  });
-
-program
-  .command('regression [env]')
-  .alias('r')
-  .description('Run regression test')
-  .option('-p, --path [value]', 'Set the path to your Stylize project root')
-  .action(function(env, options) {
-    if (options.path) {
-      projectPath = path.join(process.cwd(), options.path);
-    }
-
-    stylizeRegression.get(projectPath, function(patterns) {
-      stylizeRegression.takeScreenshot(patterns);
-    });
-  });
-
-program
-  .command('export [env]')
-  .alias('e')
-  .description('Export patterns')
-  .option('-o, --output [value]', 'Set an output path')
-  .option('-p, --path [value]', 'Set the path to your Stylize project root')
-  .action(function(env, options) {
-    var output = options.output || false;
-
-    if (options.path) {
-      projectPath = path.join(process.cwd(), options.path);
-    }
-
+  this.export = function(projectPath) {
     cliExport.run(projectPath, function() {
       log(chalk.green('Fin'));
     });
-  });
+  };
 
-program.parse(process.argv);
+  this.regression = function(projectPath) {
+    stylizeRegression.get(projectPath, function(patterns) {
+      stylizeRegression.takeScreenshot(patterns);
+    });
+  };
+};
+
+module.exports = Stylize;
